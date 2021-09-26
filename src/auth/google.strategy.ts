c096d7911f0b12be2f25d9ecf63d122820ccbf2b
Ian <chulee3110@gmail.com>
2021-09-26 18:45:33 +0900

    환경변수 적용

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -2,15 +2,16 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Profile, Strategy } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../module/users/users.service';
+import { env } from 'process';
+import { config } from 'dotenv';
 
 @Injectable()
 export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private readonly usersService: UsersService) {
     super({
       // Put config in `.env`
-      clientID:
-        '619920770218-1ln7706q3q240db5aeuk1bblee8ai7js.apps.googleusercontent.com',
-      clientSecret: 'OeVHcdIXBBGW2ee7P_PKbSpK',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
       callbackURL: 'http://localhost:3000/google/redirect',
       scope: ['email', 'profile'],
     });
