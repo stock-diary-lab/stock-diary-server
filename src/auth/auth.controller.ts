46dae21211bf7bcd241c3eceab5a6d2f93595740
Zinyon <pjy20450107@gmail.com>
2021-10-04 13:31:19 +0900

    authService 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,54 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiResponse } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 
-@Controller('google')
+@Controller()
 export class AuthController {
-  constructor(private readonly AuthService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get()
+  @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleAuth(@Req() req) {}
 
-  @Get('redirect')
+  @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
   googleAuthRedirect(@Req() req) {
-    return this.AuthService.googleLogin(req);
+    return this.authService.googleLogin(req);
+  }
+
+  @ApiResponse({
+    status: 201,
+  })
+  @Post('auth/signup/kakao')
+  singUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+
+  @ApiResponse({
+    status: 200,
+  })
+  @Post('auth/signin/kakao')
+  singIn(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.authService.signIn(createUserDto);
+  }
+
+  // token으로 가입 인증
+  @ApiResponse({
+    status: 200,
+  })
+  @Post('auth/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log('req', req);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class AuthService {
-  googleLogin(req) {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async googleLogin(req) {
     if (!req.user) {
       return 'No user from google';
     }
 
+    const payload = { name: req.user.name, email: req.user.email };
+
+    const accessToken = await this.jwtService.sign(payload);
+
     return {
       message: 'User information from google',
       user: req.user,
+      token: accessToken,
     };
+  }
+
+  async signUp(createUserDto: CreateUserDto): Promise<{ accessToken: string }> {
+    this.usersRepository.createByProvider(createUserDto);
+    const { name, email } = createUserDto;
+    const payload = { name, email };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
+  }
+
+  async signIn(createUserDto: CreateUserDto): Promise<{ accessToken: string }> {
+    const { name, email } = createUserDto;
+
+    const user = await this.usersRepository.findOne({ email });
+
+    if (user) {
+      const payload = { name, email };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
   }
 }
