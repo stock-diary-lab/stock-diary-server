a31bcb6fedeedee7b07a190563ac1c8ab97deac3
Zinyon <pjy20450107@gmail.com>
2021-10-04 13:31:42 +0900

    app module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './module/users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [TypeOrmModule.forRoot(), AuthModule],
 })
 export class AppModule {}
