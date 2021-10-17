be1439c1ab304424fc685147902e3c830ce989ae
Ian <chulee3110@gmail.com>
2021-10-17 11:26:30 +0900

    stock API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { StockModule } from './stock/stock.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, StockModule],
 })
 export class AppModule {}
---
+++ src/stock/stock.interface.ts
@@ -0,0 +1,4 @@
+export enum Type {
+  BUY = 'buy',
+  SELL = 'sell',
+}
