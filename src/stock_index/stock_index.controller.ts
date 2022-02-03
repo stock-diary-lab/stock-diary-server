6d5cc7615e1362f081de1ff699516004c2de5d2d
Zinyon <pjy20450107@gmail.com>
2022-02-03 20:57:06 +0900

    stock_index controller 주석 처리

--- src/stock_index/stock_index.controller.ts
+++ src/stock_index/stock_index.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { StockIndexService } from './stock_index.service';
-import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('stock-index')
 // @UseGuards(AuthGuard())
@@ -8,25 +7,25 @@ import { ApiResponse } from '@nestjs/swagger';
 export class StockIndexController {
   constructor(private readonly stockIndexService: StockIndexService) {}
 
-  @ApiResponse({
-    status: 200,
-    description: '지수 정보 저장 api',
-  })
-  // @ApiBearerAuth('jwt')
-  @Post()
-  async crawlAndSave() {
-    const indexes = await this.stockIndexService.crawlIndexes();
-    await this.stockIndexService.saveIndexes(indexes);
-    return { message: '크롤링 및 DB 저장 완료' };
-  }
+  // @ApiResponse({
+  //   status: 200,
+  //   description: '지수 정보 저장 api',
+  // })
+  // // @ApiBearerAuth('jwt')
+  // @Post()
+  // async crawlAndSave() {
+  //   const indexes = await this.stockIndexService.crawlIndexes();
+  //   await this.stockIndexService.saveIndexes(indexes);
+  //   return { message: '크롤링 및 DB 저장 완료' };
+  // }
 
-  @ApiResponse({
-    status: 200,
-    description: '지수 정보 조회 api',
-  })
-  @Get()
-  async getIndexes() {
-    const indexes = await this.stockIndexService.getStockIndexes();
-    return indexes;
-  }
+  // @ApiResponse({
+  //   status: 200,
+  //   description: '지수 정보 조회 api',
+  // })
+  // @Get()
+  // async getIndexes() {
+  //   const indexes = await this.stockIndexService.getStockIndexes();
+  //   return indexes;
+  // }
 }
