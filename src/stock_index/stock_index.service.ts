1b30bd0b4a3a4ba0d60395db0c15e6aeb1504162
Zinyon <pjy20450107@gmail.com>
2022-02-03 20:21:54 +0900

    pupeteer 삭제₩

--- src/stock_index/stock_index.service.ts
+++ src/stock_index/stock_index.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as puppeteer from 'puppeteer';
-import { StockIndexEntity } from './stock_index.entity';
+// import * as puppeteer from 'puppeteer';
+// import { StockIndexEntity } from './stock_index.entity';
 import { StockIndexRepository } from './stock_indexes.repository';
 
 @Injectable()
@@ -48,84 +48,84 @@ export class StockIndexService {
   //   }
   // }
 
-  async crawlIndexes() {
-    try {
-      const browser = await puppeteer.launch({ headless: true });
-      const page = await browser.newPage();
-      await page.goto('https://finance.naver.com/sise/', {
-        waitUntil: 'networkidle0',
-      });
+  // async crawlIndexes() {
+  //   try {
+  //     const browser = await puppeteer.launch({ headless: true });
+  //     const page = await browser.newPage();
+  //     await page.goto('https://finance.naver.com/sise/', {
+  //       waitUntil: 'networkidle0',
+  //     });
 
-      const koreans = await page.$$eval('.lft .tab li', (listItems) =>
-        listItems.slice(0, 2).map((listItem) => {
-          const indexName = listItem.querySelector('span.blind').textContent;
-          const currentValue = listItem.querySelector('span.num').textContent;
-          const [diff, percent] = listItem
-            .querySelector('span.num_s')
-            .textContent.split(' ');
+  //     const koreans = await page.$$eval('.lft .tab li', (listItems) =>
+  //       listItems.slice(0, 2).map((listItem) => {
+  //         const indexName = listItem.querySelector('span.blind').textContent;
+  //         const currentValue = listItem.querySelector('span.num').textContent;
+  //         const [diff, percent] = listItem
+  //           .querySelector('span.num_s')
+  //           .textContent.split(' ');
 
-          return {
-            indexName,
-            currentValue,
-            diff,
-            percent: percent.replace(/(.*\%)(.*)/g, '$1'),
-          };
-        }),
-      );
+  //         return {
+  //           indexName,
+  //           currentValue,
+  //           diff,
+  //           percent: percent.replace(/(.*\%)(.*)/g, '$1'),
+  //         };
+  //       }),
+  //     );
 
-      await page.goto('https://finance.naver.com/world');
+  //     await page.goto('https://finance.naver.com/world');
 
-      const foreigns = await page.$$eval('table#americaIndex tr', (tableRows) =>
-        tableRows
-          .map((tableRow) => {
-            const [nationEl, indexNameEl, currentValueEl, diffEl, percentEl] =
-              tableRow.children;
-            const [_, indexName, currentValue, diff, percent] = [
-              nationEl.textContent,
-              indexNameEl.textContent,
-              currentValueEl.textContent.replace(/\,/g, ''),
-              diffEl.textContent.replace(/\,/g, ''),
-              percentEl.textContent.replace(/\,/g, ''),
-            ];
+  //     const foreigns = await page.$$eval('table#americaIndex tr', (tableRows) =>
+  //       tableRows
+  //         .map((tableRow) => {
+  //           const [nationEl, indexNameEl, currentValueEl, diffEl, percentEl] =
+  //             tableRow.children;
+  //           const [_, indexName, currentValue, diff, percent] = [
+  //             nationEl.textContent,
+  //             indexNameEl.textContent,
+  //             currentValueEl.textContent.replace(/\,/g, ''),
+  //             diffEl.textContent.replace(/\,/g, ''),
+  //             percentEl.textContent.replace(/\,/g, ''),
+  //           ];
 
-            return { indexName, currentValue, diff, percent };
-          })
-          .filter((_, index) => index !== 0),
-      );
+  //           return { indexName, currentValue, diff, percent };
+  //         })
+  //         .filter((_, index) => index !== 0),
+  //     );
 
-      await browser.close();
-      return [...koreans, ...foreigns];
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  //     await browser.close();
+  //     return [...koreans, ...foreigns];
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
 
-  async saveIndexes(
-    indexes: {
-      indexName: string;
-      currentValue: string;
-      diff: string;
-      percent: string;
-    }[],
-  ) {
-    for await (const index of indexes) {
-      const found = await this.stockIndexRepository.findOne({
-        name: index.indexName,
-      });
+  // async saveIndexes(
+  //   indexes: {
+  //     indexName: string;
+  //     currentValue: string;
+  //     diff: string;
+  //     percent: string;
+  //   }[],
+  // ) {
+  //   for await (const index of indexes) {
+  //     const found = await this.stockIndexRepository.findOne({
+  //       name: index.indexName,
+  //     });
 
-      if (found) {
-        await this.stockIndexRepository.update(
-          { name: index.indexName },
-          { flucRate: index.percent, point: index.currentValue },
-        );
-      } else {
-        const newStockIndex = new StockIndexEntity({
-          name: index.indexName,
-          point: index.currentValue,
-          flucRate: index.percent,
-        });
-        await this.stockIndexRepository.save(newStockIndex);
-      }
-    }
-  }
+  //     if (found) {
+  //       await this.stockIndexRepository.update(
+  //         { name: index.indexName },
+  //         { flucRate: index.percent, point: index.currentValue },
+  //       );
+  //     } else {
+  //       const newStockIndex = new StockIndexEntity({
+  //         name: index.indexName,
+  //         point: index.currentValue,
+  //         flucRate: index.percent,
+  //       });
+  //       await this.stockIndexRepository.save(newStockIndex);
+  //     }
+  //   }
+  // }
 }
