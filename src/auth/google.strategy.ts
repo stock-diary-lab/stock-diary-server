f10faba8929f095e797ae1c029495ce1eb960b81
Zinyon <pjy20450107@gmail.com>
2022-02-05 22:19:06 +0900

    google callback url env variable로

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
       // Put config in `.env`
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/google/redirect',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ['email', 'profile'],
     });
   }
